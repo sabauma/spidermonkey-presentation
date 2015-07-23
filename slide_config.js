6ca4b403ed5b779ad73531f9ec195ba1b13351ce
Spenser Bauman <sabauma@gmail.com>
2015-07-23 14:03:21 -0700

    Initial checkin

---
+++ slide_config.js
@@ -0,0 +1,33 @@
+var SLIDE_CONFIG = {
+  // Slide settings
+  settings: {
+    title: 'Faster Polymorphism in JavaScript',
+    //subtitle: 'PhD Defense',
+    eventInfo: {
+      title: 'Mozill Intern Presentation',
+      date: 'August 30, 2015'
+    },
+    useBuilds: true, // Default: true. False will turn off slide animation builds.
+    usePrettify: true, // Default: true
+    enableSlideAreas: true, // Default: true. False turns off the click areas on either slide of the slides.
+    enableTouch: true, // Default: true. If touch support should enabled. Note: the device must support touch.
+    //analytics: 'UA-XXXXXXXX-1', // TODO: Using this breaks GA for some reason (probably requirejs). Update your tracking code in template.html instead.
+    favIcon: 'favicon.ico',
+    fonts: [
+      'Open Sans:regular,semibold,italic,italicsemibold',
+      'Source Code Pro'
+    ],
+    //theme: ['mytheme'], // Add your own custom themes or styles in /theme/css. Leave off the .css extension.
+  },
+
+  // Author information
+  presenters: [{
+    name: 'Spenser Andrew Bauman',
+    company: 'Indiana University',
+    //gplus: 'http://plus.google.com/1234567890',
+    //twitter: '@yourhandle',
+    www: 'https://sabauma.github.io',
+    github: 'https://github.com/sabauma'
+  }]
+};
+
